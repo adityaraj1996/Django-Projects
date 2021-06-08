6a814b75c2be0d081d7e48700f3a46c1e0de55b7
araj17 <aditya_raj@optum.com>
2021-06-08 21:19:43 +0530

    handling initial reload

--- urlshortner/url/urls.py
+++ urlshortner/url/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "url"
 
 urlpatterns = [
-    path("", views.urlshort, name="home"),
+    path("", views.index, name="home"),
+    path("urlshort", views.urlshort, name="urlshort"),
     path("<str:slugs>", views.urlRedirect, name="redirect")
 ]
--- urlshortner/url/views.py
+++ urlshortner/url/views.py
@@ -11,20 +11,28 @@ def index(request):
 
 
 def urlshort(request):
+    # form = Url(request.POST)
     if request.method == 'POST':
         form = Url(request.POST)
         if form.is_valid():
             slug = ''.join(random.choice(string.ascii_letters)
-                           for x in range(10))
+                               for x in range(10))
             url = form.cleaned_data["url"]
             new_url = UrlData(url=url, slug=slug)
             new_url.save()
+            return render(request, 'slug.html', {'form' : form, 'slug' : slug})
+
+
 
             # request.user.urlshort.add(new_url)
             # return redirect('/')
     else:
         form = Url()
-        slug = "INVALID URL"
+        # slug = "INVALID URL"
+        return render(request, 'index.html',{'form' : form})    
+
+
+        # slug = "INVALID URL"
 
     # data = UrlData.objects.all()
     # context = {
@@ -32,11 +40,13 @@ def urlshort(request):
     #     'data': data
     # }
     # return render(request, 'index.html', context)
-    return render(request, 'index.html', {'form' : form, 'slug' : slug})
+    # return render(request, 'index.html', {'form' : form, 'slug' : slug})
 
 
 
 # urlRedirect() — This Function tracks the slug to Original URL and redirects it to Original URL.
 def urlRedirect(request, slugs):
+    print(slugs)
     data = UrlData.objects.get(slug=slugs)
+
     return redirect(data.url)
